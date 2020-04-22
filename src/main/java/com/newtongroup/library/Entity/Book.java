9331f2b5e4797cc2cd2c7618096d7d6346b3734f
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-04-22 11:48:53 +0200

    Added Book & BookRepository

---
+++ src/main/java/com/newtongroup/library/Entity/Book.java
@@ -0,0 +1,79 @@
+package com.newtongroup.library.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idbooks")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "purchase_price")
+    private int purchasePrice;
+
+    public Book () {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(int purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+}
---
+++ src/main/java/com/newtongroup/library/Repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.newtongroup.library.Repository;
+
+import com.newtongroup.library.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
