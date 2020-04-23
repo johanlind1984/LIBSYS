bd5b461e2070d0cd449998d9bd99a04f7b57186b
Johanna Axnér <johanna.k.axner@gmail.com>
2020-04-23 14:12:51 +0200

    Update Abstract book-author

--- src/main/java/com/newtongroup/library/Entity/Book.java
+++ src/main/java/com/newtongroup/library/Entity/Book.java
@@ -1,7 +1,8 @@
 package com.newtongroup.library.Entity;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "books")
@@ -20,73 +21,4 @@ public class Book extends AbstractBook {
 	}
 
 
-
-
-
-	/*
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "book_id")
-    private int bookId;
-
-    @Column(name = "title")
-    private String title;
-
-    @Column(name = "isbn")
-    private String isbn;
-
-    @Column(name = "publisher")
-    private String publisher;
-
-    @Column(name = "purchase_price")
-    private int purchasePrice;
-
-    @ManyToMany (mappedBy = "bookList")
-    private List<Author> authorList;
-
-
-    public Book () {
-    }
-
-    public int getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(int id) {
-        this.bookId = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public int getPurchasePrice() {
-        return purchasePrice;
-    }
-
-    public void setPurchasePrice(int purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
-
-    */
 }
