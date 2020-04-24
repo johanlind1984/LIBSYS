06c947b6ac5857fdfdbd96ce9f105f56362c52c5
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-04-24 14:05:44 +0200

    Skapat authorController
    Skapat lite grund för författare, endast backend.

---
+++ src/main/java/com/newtongroup/library/Controller/AuthorController.java
@@ -0,0 +1,58 @@
+package com.newtongroup.library.Controller;
+
+import com.newtongroup.library.Entity.Author;
+import com.newtongroup.library.Repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/author")
+public class AuthorController {
+
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    private String header = "librarian/librarianheader.html";
+
+    @RequestMapping("/")
+    public String mainView(Model model) {
+        model.addAttribute("header", new String(header));
+        return "librarian/start";
+    }
+
+    @GetMapping("/new")
+    public String NewAuthorForm(Model model) {
+
+        Author author = new Author();
+
+        model.addAttribute("author", author);
+
+        return "librarian/start";
+
+    }
+
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ModelAndView saveAuthor(Model model,
+                                  @RequestParam("firstName") String firstName,
+                                  @RequestParam("lastName") String lastName) {
+
+        Author author = (Author) authorRepository.findAll();
+
+        if (author.getFirstname().equals(firstName) || author.getLastname().equals(lastName)) {
+            System.out.println("Författaren finns redan i registret.");
+        } else {
+
+            author.setFirstname(firstName);
+            author.setLastname(lastName);
+
+            authorRepository.save(author);
+        }
+        return new ModelAndView("redirect:/author/");
+    }
+}
