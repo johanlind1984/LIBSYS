05d37468e4c46849c63cab13462c346f0b491a54
Johanna Axnér <johanna.k.axner@gmail.com>
2020-04-29 14:20:50 +0200

    Städade kod från kommentarer

--- src/main/java/com/newtongroup/library/Entity/AbstractBook.java
+++ src/main/java/com/newtongroup/library/Entity/AbstractBook.java
@@ -1,12 +1,9 @@
 package com.newtongroup.library.Entity;
 
-import java.util.List;
+import org.hibernate.search.annotations.Field;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.IndexedEmbedded;
 
 @MappedSuperclass
 public class AbstractBook extends AbstractRental {
@@ -15,12 +12,6 @@ public class AbstractBook extends AbstractRental {
 	@Field
 	private String isbn;
 
-//	@ManyToMany
-//	@JoinTable(
-//			name="book_author",
-//			joinColumns = {@JoinColumn(name="idbook_author_book_id")},
-//			inverseJoinColumns = {@JoinColumn(name="idbook_author_author_id")})
-//	private List<Author>authorList;
 
 	public AbstractBook() {
 
@@ -35,12 +26,6 @@ public class AbstractBook extends AbstractRental {
 		this.isbn = isbn;
 	}
 
-//	public List<Author> getAuthorList() {
-//		return authorList;
-//	}
-//
-//	public void setAuthorList(List<Author> authorList) {
-//		this.authorList = authorList;
-//	}
+
 
 }
