e162250445422374a70fa5acc4114b6ba16ffcab
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-05-02 03:50:22 +0200

    Updates

--- src/main/java/com/newtongroup/library/Controller/indexController.java
+++ src/main/java/com/newtongroup/library/Controller/indexController.java
@@ -1,9 +1,10 @@
-package com.newtongroup.library.Service;
+package com.newtongroup.library.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
-public class index {
+@Controller
+public class indexController {
 
     @RequestMapping("/")
     public String indexPage() {
