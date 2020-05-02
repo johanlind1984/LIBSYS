4b66e143e692dbfd08526d80ece8871c81c48d83
Rimanda18 <zivkovicrima@gmail.com>
2020-05-02 19:45:12 +0200

    css och header i sök

--- src/main/java/com/newtongroup/library/Controller/SearchServiceController.java
+++ src/main/java/com/newtongroup/library/Controller/SearchServiceController.java
@@ -1,7 +1,9 @@
 package com.newtongroup.library.Controller;
 
-import java.util.List;
-
+import com.newtongroup.library.Entity.Book;
+import com.newtongroup.library.Repository.UserRepository;
+import com.newtongroup.library.Service.SearchService;
+import com.newtongroup.library.Utils.HeaderUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.newtongroup.library.Entity.Book;
-import com.newtongroup.library.Service.SearchService;
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/search")
@@ -18,10 +20,13 @@ public class SearchServiceController {
 	
 	@Autowired
 	private SearchService searchService;
+
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping()
-	public String searchForm( @RequestParam(value="search", required =false) String searchText, Model model) {
-		
+	public String searchForm(@RequestParam(value="search", required =false) String searchText, Model model, Principal principal) {
+		model.addAttribute("header", HeaderUtils.getHeaderString(userRepository.findByUsername(principal.getName())));
 		if (searchText != null && !searchText.isEmpty() && !searchText.isBlank()) {
 			
 			List<Book> result = searchService.searchBooks(searchText);
