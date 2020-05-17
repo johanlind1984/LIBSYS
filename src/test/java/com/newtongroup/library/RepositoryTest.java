e11ae109b6fe85cb613455fa557027ec00263131
Rimanda18 <zivkovicrima@gmail.com>
2020-05-17 10:27:59 +0200

    Update RepositoryTest.java

--- src/test/java/com/newtongroup/library/RepositoryTest.java
+++ src/test/java/com/newtongroup/library/RepositoryTest.java
@@ -1,8 +1,11 @@
 package com.newtongroup.library;
 
-import com.newtongroup.library.Entity.*;
+import com.newtongroup.library.Entity.Author;
+import com.newtongroup.library.Entity.Book;
+import com.newtongroup.library.Entity.Placement;
+import com.newtongroup.library.Entity.User;
 import com.newtongroup.library.Repository.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
