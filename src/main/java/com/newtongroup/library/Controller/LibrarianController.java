d0ca63700af35199368d903ebdb3bdfd1afe7fb8
Sebasjun <sebastianjunhemschool@gmail.com>
2020-05-18 21:23:38 +0200

    Sök på låna/återlämning/spärr

--- src/main/java/com/newtongroup/library/Controller/LibrarianController.java
+++ src/main/java/com/newtongroup/library/Controller/LibrarianController.java
@@ -6,11 +6,13 @@ import com.newtongroup.library.Utils.HeaderUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -74,16 +76,31 @@ public class LibrarianController {
     private String prepareToReturnBook(Model theModel, Principal principal) {
         System.out.println(principal.getName());
         theModel.addAttribute("header", HeaderUtils.getHeaderString(userRepository.findByUsername(principal.getName())));
+
+      List<Book> bookList = getActiveBookList();
+
+
+        Book book = new Book();
+
+
+
+        theModel.addAttribute("bookList", bookList);
+        theModel.addAttribute("book", book);
+
         return "loan/return-book";
     }
 
     @RequestMapping("/return-book")
-    private String returnBook(@RequestParam(name="bookId", required = false) Long bookId,
+    private String returnBook(@RequestParam(name="bookId", required = false) Long bookIdTEST,
                               @RequestParam(name="eBookId", required = false) Long eBookId,
+                              @ModelAttribute("book") Book book,
+                              @ModelAttribute("bookLoan") BookLoan bookLoan,
                               Model theModel, Principal principal) {
 
         theModel.addAttribute("header", HeaderUtils.getHeaderString(userRepository.findByUsername(principal.getName())));
 
+        Long bookId = bookLoan.getBook().getId();
+        System.out.println(bookId);
 
         if(bookId != null) {
             Book bookToReturn = bookrepository.findById(bookId).orElse(null);
@@ -116,6 +133,18 @@ public class LibrarianController {
         }
 
         return "loan/return-success";
+    }
+
+    private List<Book> getActiveBookList(){
+        List<Book> tempList = bookrepository.findAll();
+        List<Book> bookList = new ArrayList<>();
+
+        for(Book temp : tempList){
+            if(!temp.isAvailable()){
+                bookList.add(temp);
+            }
+        }
+        return bookList;
     }
 
 
