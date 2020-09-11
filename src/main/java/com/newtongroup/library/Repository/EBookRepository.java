76aba13312026bb4a6deb6ae4f8d5822f4480876
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-09-11 10:33:17 +0200

    Removed Imports

--- src/main/java/com/newtongroup/library/Repository/EBookRepository.java
+++ src/main/java/com/newtongroup/library/Repository/EBookRepository.java
@@ -1,9 +1,8 @@
 package com.newtongroup.library.Repository;
 
+import com.newtongroup.library.Entity.EBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.newtongroup.library.Entity.EBook;
 
 @Repository
 public interface EBookRepository extends JpaRepository<EBook, Long> {
--- src/main/java/com/newtongroup/library/Repository/VisitorRepository.java
+++ src/main/java/com/newtongroup/library/Repository/VisitorRepository.java
@@ -1,9 +1,6 @@
 package com.newtongroup.library.Repository;
 
-import com.newtongroup.library.Entity.Admin;
-import com.newtongroup.library.Entity.Person;
 import com.newtongroup.library.Entity.Visitor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
